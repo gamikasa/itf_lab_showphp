613e766b0bcc7713999948c8516e6ee7992e7ee8
gamikasa <73681995+gamikasa@users.noreply.github.com>
2020-11-13 12:54:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -54,19 +54,9 @@ mysqli_close($conn);
     </thead>
     <tbody>
       <tr>
-        <td>John</td>
+        <td><?php echo $Result['Name'];?></div></td>
         <td>Doe</td>
         <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
       </tr>
     </tbody>
   </table>
