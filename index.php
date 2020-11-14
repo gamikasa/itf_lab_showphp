3b1eed2bd2f92364da5d5f4be5c013764d0702df
gamikasa <73681995+gamikasa@users.noreply.github.com>
2020-11-14 17:03:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <html>
 <head>
-	<title>ITF Lab</title>
-	<title>Bootstrap Example</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<title>ITF Lab</title>
 </head>
 <body>
 <?php
@@ -41,23 +34,5 @@ while($Result = mysqli_fetch_array($res))
 <?php
 mysqli_close($conn);
 ?>
-<div class="container">          
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td><?php echo $Result['Name'];?></div></td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
 </body>
 </html>
