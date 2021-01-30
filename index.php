3eac5f7e1ce331a48c89425520ec792c4320884a
gamikasa <73681995+gamikasa@users.noreply.github.com>
2021-01-30 09:23:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'ramsserver.mysql.database.azure.com', 'gamikasa@ramsserver', 'ram-129876543210', 'itflab', 3306);
+mysqli_real_connect($conn, 'ramsserver.mysql.database.azure.com', 'gamikasa@ramsserver', 'passwordja', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
